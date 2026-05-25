860c8ccd90ca7ac94e7765b2ad47e1fc0be09f91
Prodeon <prodeon21@gmail.com>
2026-05-25 13:33:48 +0300

    add check for instance in validate method

--- habits/serializer.py
+++ habits/serializer.py
@@ -2,6 +2,9 @@ from rest_framework.serializers import ModelSerializer
 from habits.models import UsefulHabit, PleasantHabit, Place
 from rest_framework import serializers
 
+from users.serializers import CustomUserSerializer
+
+
 class UsefulHabitSerializer(ModelSerializer):
     class Meta:
         model = UsefulHabit
@@ -9,29 +12,34 @@ class UsefulHabitSerializer(ModelSerializer):
         read_only_fields = ["user"]
 
     def validate(self, data):
-        text_reward = data.get('text_reward')
-        pleasant_reward = data.get('pleasant_habit_reward')
 
-        errors = {}
+        if self.instance is None:
 
-        if text_reward and pleasant_reward:
-            errors['non_field_errors'] = [
-                "Заполните только одно поле: text_reward ИЛИ pleasant_habit_reward"
-            ]
-        elif not text_reward and not pleasant_reward:
-            errors['non_field_errors'] = [
-                "Необходимо заполнить либо text_reward, либо pleasant_habit_reward"
-            ]
+            text_reward = data.get('text_reward')
+            pleasant_reward = data.get('pleasant_habit_reward')
 
-        if errors:
-            raise serializers.ValidationError(errors)
+            errors = {}
 
+            if text_reward and pleasant_reward:
+                errors['non_field_errors'] = [
+                    "Заполните только одно поле: text_reward ИЛИ pleasant_habit_reward"
+                ]
+            elif not text_reward and not pleasant_reward:
+                errors['non_field_errors'] = [
+                    "Необходимо заполнить либо text_reward, либо pleasant_habit_reward"
+                ]
+
+            if errors:
+                raise serializers.ValidationError(errors)
+
+            return data
         return data
 
 class PleasantHabitSerializer(ModelSerializer):
     class Meta:
         model = PleasantHabit
         fields = "__all__"
+        read_only_fields = ["user"]
 
 class PlaceSerializer(ModelSerializer):
     class Meta:
