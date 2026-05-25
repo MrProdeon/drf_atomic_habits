da50e1e4edf994d415e204b91a1dcb24c839a60d
Prodeon <prodeon21@gmail.com>
2026-05-25 11:50:41 +0300

    add usermanager for set_password

--- users/models.py
+++ users/models.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None, **extra_fields):
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
 # Create your models here.
 class CustomUser(AbstractUser):
-
+    objects = CustomUserManager()
     def __str__(self):
         return self.username
