223dc872e9f606e00123b43e6ec40d465d9c97c4
Prodeon <prodeon21@gmail.com>
2026-05-25 13:34:13 +0300

    add urls for retrieve, update and destroy for habits

--- habits/urls.py
+++ habits/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import UserHabitsAPIView, CreateUsefulHabitAPIView, CreatePleasantHabitAPIView, PlaceCreateAPIView
+from habits.views import UserHabitsAPIView, CreateUsefulHabitAPIView, CreatePleasantHabitAPIView, PlaceCreateAPIView, \
+    UsefulHabitRetrieveUpdateDestroyAPIView, PleasantHabitRetrieveUpdateDestroyAPIView
 
 app_name = HabitsConfig.name
 
@@ -10,6 +11,8 @@ urlpatterns = [
     path("my_habits/", UserHabitsAPIView.as_view(), name="user-habits"),
     path("create/useful_habit/", CreateUsefulHabitAPIView.as_view(), name="create-useful-habit"),
     path("create/pleasant_habit/", CreatePleasantHabitAPIView.as_view(), name="create-pleasant-habit"),
-    path("create/place/", PlaceCreateAPIView.as_view(), name="create-place")
+    path("create/place/", PlaceCreateAPIView.as_view(), name="create-place"),
+    path("useful_habit/<int:pk>/", UsefulHabitRetrieveUpdateDestroyAPIView.as_view(), name="useful-habit"),
+    path("pleasant_habit/<int:pk>/", PleasantHabitRetrieveUpdateDestroyAPIView.as_view(), name="pleasant-habit")
 
 ]
