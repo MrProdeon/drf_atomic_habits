af06da33d64349e0e18c8ef245592a496c10a2a0
Prodeon <prodeon21@gmail.com>
2026-05-28 12:22:34 +0300

    add urls for fixed views

--- habits/urls.py
+++ habits/urls.py
@@ -2,16 +2,18 @@ from django.contrib import admin
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import UserHabitsAPIView, CreateUsefulHabitAPIView, CreatePleasantHabitAPIView, PlaceCreateAPIView, \
+from habits.views import UserUsefulHabitsAPIView, UserPleasantsAPIView, CreateUsefulHabitAPIView, CreatePleasantHabitAPIView, PlaceCreateAPIView, \
     UsefulHabitRetrieveUpdateDestroyAPIView, PleasantHabitRetrieveUpdateDestroyAPIView, PublicHabitsAPIView, \
-    PlaceRetrieveUpdateDestroyAPIView
+    PlaceRetrieveUpdateDestroyAPIView, PublicUsefulHabits, PublicPleasantHabits
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     # habits
-    path("my_habits/", UserHabitsAPIView.as_view(), name="user-habits"),
-    path("public_habits/", PublicHabitsAPIView.as_view(), name="public-habits"),
+    path("user_useful_habits/", UserUsefulHabitsAPIView.as_view(), name="user-useful-habits"),
+    path("user_pleasant_habits/", UserPleasantsAPIView.as_view(), name="user-pleasant-habits"),
+    path("public_useful_habits/", PublicUsefulHabits.as_view(), name="public-useful-habits"),
+    path("public_pleasant_habits/", PublicPleasantHabits.as_view(), name="public-pleasant-habits"),
     path("create/useful_habit/", CreateUsefulHabitAPIView.as_view(), name="create-useful-habit"),
     path("create/pleasant_habit/", CreatePleasantHabitAPIView.as_view(), name="create-pleasant-habit"),
     path("useful_habit/<int:pk>/", UsefulHabitRetrieveUpdateDestroyAPIView.as_view(), name="useful-habit"),
