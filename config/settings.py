4d749076e8e0b0f53c06db844a6cb3f9bfc46a1b
Prodeon <prodeon21@gmail.com>
2026-05-29 14:56:35 +0300

    add second task in settngs

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
@@ -32,7 +31,6 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -83,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
@@ -97,7 +94,6 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -117,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
@@ -126,7 +121,6 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
@@ -151,8 +145,12 @@ SIMPLE_JWT = {
 }
 
 CELERY_BEAT_SCHEDULE = {
-    'task-name': {
-        'task': 'habits.tasks.habit_reminder',
+    'useful_habit_reminder': {
+        'task': 'habits.tasks.useful_habit_reminder',
+        'schedule': timedelta(minutes=1),
+    },
+    'pleasant_habit_reminder': {
+        'task': 'habits.tasks.pleasant_habit_reminder',
         'schedule': timedelta(minutes=1),
     },
 }
