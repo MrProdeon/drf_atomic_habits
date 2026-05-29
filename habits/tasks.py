1019427bba844cb818c14404431933de63b57b78
Prodeon <prodeon21@gmail.com>
2026-05-29 13:29:48 +0300

    little fix in message_text

--- habits/tasks.py
+++ habits/tasks.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 
 from habits.services import send_message_in_telegram
 
+
 @shared_task
 def useful_habit_reminder():
     now = timezone.now()
@@ -14,15 +15,12 @@ def useful_habit_reminder():
     habits_to_notify = UsefulHabit.objects.filter(
         next_notification__isnull=False,
         next_notification__lte=notification_time,
-        next_notification__gte=now-timedelta(5)
+        next_notification__gte=now - timedelta(5)
     )
 
     for habit in habits_to_notify:
         try:
-            message_text = f"""Напоминание о выполнении привычки.
-Действие - {habit.action}
-Место - {habit.place.name}
-Время - {habit.time_for_habit}"""
+            message_text = f"Напоминание о привычке\nДействие - {habit.action}\nМесто - {habit.place.name}\nВремя - {habit.time_for_habit}"
             send_message_in_telegram(chat_id=habit.user.tg_chat_id,
                                      message_text=message_text)
             habit.update_after_notification()
