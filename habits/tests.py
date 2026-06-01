8470d2dce9bbf595b332a16a4311993698251a02
Prodeon <prodeon21@gmail.com>
2026-06-01 17:36:46 +0300

    add test CreateHabitsTestCase

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from users.models import CustomUser
+from habits.models import Place
+
 
 # Create your tests here.
+class CreateHabitsTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(username="testuser", password="testpassword")
+        self.client.force_authenticate(user=self.user)
+
+        self.place = Place.objects.create(name="test_place", owner=self.user)
+
+        self.useful_url = reverse("habits:create-useful-habit")
+        self.useful_data = {
+            "time_for_habit": "12:00",
+            "action": "test action",
+            "periodicity": 1,
+            "lead_time": 100,
+            "is_public": True,
+            "place": self.place.id,
+            "text_reward": "test rewatd"
+        }
+
+        self.pleasant_url = reverse("habits:create-pleasant-habit")
+        self.pleasant_data = {
+            "time_for_habit": "21:00",
+            "action": "pleasant action",
+            "periodicity": 1,
+            "lead_time": 100,
+            "is_public": False,
+            "place": self.place.id,
+        }
+
+    def test_post(self):
+        useful_response = self.client.post(self.useful_url, self.useful_data)
+        parse_useful_response = useful_response.json()
+        self.assertEqual(useful_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(parse_useful_response["action"], "test action")
+        self.assertEqual(parse_useful_response["place"], 1)
+        self.assertEqual(parse_useful_response["is_public"], True)
+
+        pleasant_response = self.client.post(self.pleasant_url, self.pleasant_data)
+        parse_pleasant_response = pleasant_response.json()
+        self.assertEqual(pleasant_response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(parse_pleasant_response["action"], "pleasant action")
+        self.assertEqual(parse_pleasant_response["time_for_habit"], "21:00:00")
+        self.assertEqual(parse_pleasant_response["is_public"], False)
+
